3816cec5f8a6cc77b958b8fdf699f1f52afd64c6
Vasily Yanichkin <vas-yan2007@yandex.ru>
2022-01-26 22:09:17 +0300

    Update to Task 16 added

--- HelloPython/16.py
+++ HelloPython/16.py
@@ -6,15 +6,13 @@ Wednesday = 4
 Thursday = 5
 Friday = 6
 Saturday = 7
-from operator import truediv
-from tabnanny import check
 import Random_Numbers
 given_number = Random_Numbers.get_random(1,7)
-def check_wether_weekend(number):
+def check_whether_weekend(number):
     result = True
     if given_number == 1 or given_number == 7:
         result = True
     else:
         result = False
     return result
-print(given_number, check_wether_weekend(given_number))
+print(given_number, check_whether_weekend(given_number))
