2be593c3be40a9a69b97829800ae90cf7fa7d03b
Vasily Yanichkin <vas-yan2007@yandex.ru>
2022-01-28 20:42:28 +0300

    update to the code added

--- HelloPython/New_List_14.py
+++ HelloPython/New_List_14.py
@@ -3,8 +3,8 @@ a = 8.11111
 def sum_of_digits(a):
     result = 0
     b = str(a)
-    for i in range (0, len(b)):
-        if b[i] != ".":
-            result += int(b[i])
+    for i in b:
+        if i != ".":
+            result += int (i)
     return result
-print(a, sum_of_digits(a))
+print(a,sum_of_digits(a))
